97da0785e3e228b5d4f8360f0654a1da982ea129
Francisco Melo <franciscomelo@oi.net.br>
2019-04-02 19:04:33 -0300

    eslint airbnb friendly

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const restify = require("restify");
+const restify = require('restify');
 
 respond = (req, res, next) => {
-  console.log("responding call " + req.params.name);
-  res.send("hello " + req.params.name);
-  next();
+    console.log(`responding call ${req.params.name}`);
+    res.send(`hello ${req.params.name}`);
+    next();
 };
 
 const server = restify.createServer();
-server.get("/hello/:name", respond);
-server.head("/hello/:name", respond);
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
 
-server.listen(process.env.PORT || 8080, () =>
-  console.log("%s listening at %s", server.name, server.url)
-);
+server.listen(process.env.PORT || 8080, () => console.log('%s listening at %s', server.name, server.url));
